7ff79c4ead2d9f12c836286d030d714ef4d41d30
Joseph Kim <josephkim@Josephs-MacBook-Air.local>
2024-04-03 18:15:03 -0400

    Added another test

--- test.py
+++ test.py
@@ -39,5 +39,25 @@ class TestStringMethods(unittest.TestCase):
         eng_to_rads = create_eng_to_rads(char_to_rad, eng_to_char)
         self.assertEqual(eng_to_rads["hello"], ["rad1", "rad2", "rad3", "rad4"])
 
+    def test_eng_to_rads2(self):
+        # test the create_eng_to_rads function
+        eng_to_char = {
+            "hello": ["h", "e", "l", "l", "o"],
+            "world": ["w", "o", "r", "l", "d"]
+        }
+
+        char_to_rad = {
+            "h": ["rad1", "rad8"],
+            "e": ["rad2"],
+            "l": ["rad3"],
+            "o": ["rad4"],
+            "w": ["rad5"],
+            "r": ["rad6"],
+            "d": ["rad7"]
+        }
+
+        eng_to_rads = create_eng_to_rads(char_to_rad, eng_to_char)
+        self.assertEqual(eng_to_rads["hello"], ["rad1", "rad8", "rad2", "rad3", "rad4"])
+
 if __name__ == '__main__':
     unittest.main()
