c181df0c4f2043324188c77f0df93ea122a461b1
Joseph Kim <josephkim@Josephs-MacBook-Air.local>
2024-04-03 18:04:25 -0400

    Edited to use the better method in utils

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import torch
+import utils
 
 DATA_DIRECTORY_NAME = "data"
 CHAR_TO_RAD_FILENAME = "char_to_rad.json"
@@ -35,15 +36,9 @@ def create_eng_to_rads(char_to_rads, eng_to_chars):
 
     return eng_to_rads
 
-def load_json_dict(filename):
-    # Check if we need to modify the open method because of utf stuff!
-    with open(filename) as file:
-        json_dict = json.load(file)
-    return json_dict
-
 def load_eng_to_rads():
-    char_to_rads = load_json_dict(CHAR_TO_RAD_DIRECTORY)
-    eng_to_chars = load_json_dict(ENG_TO_CHARS_DIRECTORY)
+    char_to_rads = utils.json_to_dict(CHAR_TO_RAD_DIRECTORY)
+    eng_to_chars = utils.json_to_dict(ENG_TO_CHARS_DIRECTORY)
     eng_to_rads = create_eng_to_rads(char_to_rads, eng_to_chars)
     return eng_to_rads
 
