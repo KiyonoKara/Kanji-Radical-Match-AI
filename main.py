bfe3c3bed72b66b8207634a3ea5c0ba445a74e8b
Joseph Kim <josephkim@Josephs-MacBook-Air.local>
2024-04-03 17:19:04 -0400

    Starting initial structure

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import json
 
+CHAR_TO_RAD_FILENAME = "char_to_rad.json"
+ENG_TO_CHARS_FILENAME = "eng_to_chars.json"
+
+
+
+def load_files():
+    char_to_rads = load_dict(CHAR_TO_RAD_FILENAME)
+    eng_to_chars = load_dict(ENG_TO_CHARS_FILENAME)
+    eng_to_rads = create_eng_to_rads(char_to_rads, eng_to_chars)
+    
+def load_dict(filename):
+    # Check if we need to modify the open method because of utf stuff!
+    with open(filename) as file:
+        dict = json.load(file)
+    return dict
+
+def main():
+    load_files()
+
+
+
+if __name__ == "__main__":
+    main()
