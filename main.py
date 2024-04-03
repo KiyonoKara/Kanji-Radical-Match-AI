19fc63ed34e91b454696aa858dd31396e0f13e3e
Joseph Kim <josephkim@Josephs-MacBook-Air.local>
2024-04-03 17:33:35 -0400

    Skeleton code complete

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 import json
+import numpy as np
 
+DATA_DIRECTORY_NAME = "data"
 CHAR_TO_RAD_FILENAME = "char_to_rad.json"
+CHAR_TO_RAD_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{CHAR_TO_RAD_FILENAME}"
 ENG_TO_CHARS_FILENAME = "eng_to_chars.json"
+ENG_TO_CHARS_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{ENG_TO_CHARS_FILENAME}"
 
+def run_model(word, model):
+    # throw word in model to see the radical vector
+    radical_vect = []
+    return radical_vect
 
+def train_model(eng_to_rads):
+    # use the english to radicals dictionary to train the model
+    model = []
+    return model
 
-def load_files():
-    char_to_rads = load_dict(CHAR_TO_RAD_FILENAME)
-    eng_to_chars = load_dict(ENG_TO_CHARS_FILENAME)
-    eng_to_rads = create_eng_to_rads(char_to_rads, eng_to_chars)
-    
-def load_dict(filename):
+def train_word(word_vec, true_lbl):
+    # train a single word
+    return
+
+def save_model(model, filename):
+    output_directory = f"{DATA_DIRECTORY_NAME}/{filename}"
+    # save model
+    return
+
+def create_eng_to_rads(char_to_rads, eng_to_chars):
+    eng_to_rads = dict()
+
+    # TODO: use the character to radical dictionary and english to
+    # character dictionary to construct the english to radical dictionary
+
+    return eng_to_rads
+
+def load_json_dict(filename):
     # Check if we need to modify the open method because of utf stuff!
     with open(filename) as file:
-        dict = json.load(file)
-    return dict
+        json_dict = json.load(file)
+    return json_dict
+
+def load_eng_to_rads():
+    char_to_rads = load_json_dict(CHAR_TO_RAD_DIRECTORY)
+    eng_to_chars = load_json_dict(ENG_TO_CHARS_DIRECTORY)
+    eng_to_rads = create_eng_to_rads(char_to_rads, eng_to_chars)
+    return eng_to_rads
 
 def main():
-    load_files()
-
-
+    eng_to_rads = load_eng_to_rads()
 
 if __name__ == "__main__":
     main()
