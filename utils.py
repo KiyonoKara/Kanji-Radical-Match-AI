e456c0e1b907c43f5d41a510a676ce84e695ca0b
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2024-04-04 17:17:20 -0400

    Kanji Feedforward NN so far

--- utils.py
+++ utils.py
@@ -1,10 +1,13 @@
 import json
+import torch.nn as nn
+import torch.nn.functional as F
 
 DATA_DIRECTORY_NAME = "data"
 CHAR_TO_RAD_FILENAME = "kanji_to_radical.json"
 CHAR_TO_RAD_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{CHAR_TO_RAD_FILENAME}"
 ENG_TO_CHARS_FILENAME = "english_to_kanji.json"
 ENG_TO_CHARS_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{ENG_TO_CHARS_FILENAME}"
+
 
 def json_to_dict(json_file: str) -> dict:
     """
@@ -49,3 +52,24 @@ def load_eng_to_rads() -> dict[str, list[str]]:
     eng_to_kanji = json_to_dict(ENG_TO_CHARS_DIRECTORY)
     eng_to_rads = create_eng_to_rads(kanji_to_rads, eng_to_kanji)
     return eng_to_rads
+
+
+class KanjiFFNN(nn.Module):
+    def __init__(self, eng_vocab_size: int, radical_vocab_size: int, nodes: int):
+        super(KanjiFFNN, self).__init__()
+        # Hidden layer
+        self.hid1 = nn.Linear(eng_vocab_size, nodes)
+        # Output layer
+        self.out1 = nn.Linear(nodes, radical_vocab_size)
+
+    def forward(self, x):
+        # Pass input x to hidden layer
+        x = self.hid1(x)
+        # Apply ReLU activation function to output of first layer
+        x = F.relu(x)
+        # Pass the output from the previous layer to the output layer
+        x = self.out1(x)
+        # Apply sigmoid to output
+        x = F.sigmoid(x)
+        return x
+
