e726f1094b949b8c22103d4edccd74fc65911670
Kiyo <68307546+KiyonoKara@users.noreply.github.com>
2024-04-03 23:53:29 -0400

    Document create_eng_to_rads function, will be moved in next commit

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ CHAR_TO_RAD_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{CHAR_TO_RAD_FILENAME}"
 ENG_TO_CHARS_FILENAME = "english_to_kanji.json"
 ENG_TO_CHARS_DIRECTORY = f"{DATA_DIRECTORY_NAME}/{ENG_TO_CHARS_FILENAME}"
 
+
 # converted dictionary to pandas data frame so we can easily read the json
 def reformat_data():
     eng_to_chars_data = pd.read_json(ENG_TO_CHARS_DIRECTORY)
@@ -25,6 +26,7 @@ def reformat_data():
         eng_to_chars_data = eng_to_chars_data.explode("Kanji")
     return eng_to_chars_data
 
+
 # encodes the data and labels from strings to randomly assigned numbers so they can be tensorized
 # then convert data and labels to tensors
 def preprocess_data(data):
@@ -35,6 +37,7 @@ def preprocess_data(data):
     label_tensor = torch.tensor(encoded_label)
     return data_tensor, label_tensor
 
+
 # model is a list of np vectors
 
 def run_model(word, model):
@@ -42,36 +45,44 @@ def run_model(word, model):
     radical_vect = []
     return radical_vect
 
+
 def train_model(eng_to_rads):
     # use the english to radicals dictionary to train the model
-
+    model = '''Add later'''
+    # Dev note: Might recommend building a class-style torch feedforward model
     return model
+
 
 def train_word(word_vec, true_lbl):
     # train a single word
     return
 
+
 def save_model(model, filename):
     torch.save(model, filename)
 
-def create_eng_to_rads(char_to_rads, eng_to_chars):
+
+def create_eng_to_rads(kanji_to_rads, eng_to_kanji) -> dict[str, list[str]]:
+    """
+    Use the kanji to radical dictionary and English to
+    Character dictionary to construct the English to radical dictionary
+    :param kanji_to_rads:
+    :param eng_to_kanji:
+    :return: dict of English words to radicals
+    """
+
     eng_to_rads = dict()
-
-    # use the character to radical dictionary and english to
-    # character dictionary to construct the english to radical dictionary
-
-    for eng_word in eng_to_chars:
-
-        # create new dict entry for eng word
+    for eng_word in eng_to_kanji:
+        # Create new dict entry for English word
         eng_to_rads[eng_word] = []
-        for char in eng_to_chars[eng_word]:
-
-            # add unique radicals to eng word entry
-            if char in char_to_rads:
-                for rad in char_to_rads[char]:
-                    if (rad not in eng_to_rads[eng_word]):
+        for kanji in eng_to_kanji[eng_word]:
+            # Add unique radicals to English word entry
+            if kanji in kanji_to_rads:
+                for rad in kanji_to_rads[kanji]:
+                    if rad not in eng_to_rads[eng_word]:
                         eng_to_rads[eng_word].append(rad)
     return eng_to_rads
+
 
 def load_eng_to_rads():
     char_to_rads = utils.json_to_dict(CHAR_TO_RAD_DIRECTORY)
@@ -79,9 +90,11 @@ def load_eng_to_rads():
     eng_to_rads = create_eng_to_rads(char_to_rads, eng_to_chars)
     return eng_to_rads
 
+
 def main():
     return
     # put in whatever you want to here for debugging locally
+
 
 if __name__ == "__main__":
     main()
